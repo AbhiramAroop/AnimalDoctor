6e9da906ff19407f3582f43cfafcb9e6042863cf
aaro0006 <aaro0006@MVAZ2STUL01048.ad.monash.edu>
2021-05-09 16:01:46 +1000

    Task 2b Completed and tested

--- Assignments/Ass2A/StudentFiles/T2-cb-dm.sql
+++ Assignments/Ass2A/StudentFiles/T2-cb-dm.sql
@@ -58,5 +58,36 @@ INSERT into ANIMAL (ANIMAL_ID,ANIMAL_SEX,CENTRE_ID,BREVENT_ID,SPEC_GENUS,SPEC_NA
 
 -- (iv)
 
+Insert into BREEDING_EVENT (BREVENT_ID,BREVENT_DATE,MOTHER_ID,FATHER_ID) values (brevent_id_seq.nextval,
 
+                                                                                '10-Feb-2021',
+                                                                                
+                                                                                (SELECT animal.animal_id FROM animal WHERE animal.centre_id = (SELECT centre.centre_id FROM centre WHERE centre.centre_name = 'Australia Zoo')
+                                                                                AND animal.spec_genus = (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil')
+                                                                                AND animal.spec_name = (SELECT species.spec_name FROM species WHERE species.spec_popular_name = 'Tasmanian Devil')),
+                                                                                
+                                                                                (SELECT animal.animal_id FROM animal WHERE animal.centre_id = (SELECT centre.centre_id FROM centre WHERE centre.centre_name = 'Werribee Open Range Zoo')
+                                                                                AND animal.spec_genus = (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil')
+                                                                                AND animal.spec_name = (SELECT species.spec_name FROM species WHERE species.spec_popular_name = 'Tasmanian Devil')));
 
+Insert into ANIMAL (ANIMAL_ID,ANIMAL_SEX,CENTRE_ID,BREVENT_ID,SPEC_GENUS,SPEC_NAME) values (animal_id_seq.nextval,
+                                                                                            'F',
+                                                                                            (SELECT centre.centre_id FROM centre WHERE centre.centre_name = 'Australia Zoo'),
+                                                                                            (SELECT breeding_event.brevent_id FROM breeding_event WHERE breeding_event.brevent_date = '10-Feb-2021'
+                                                                                                AND breeding_event.mother_id = (SELECT animal.animal_id FROM animal WHERE animal.spec_genus = (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil') 
+                                                                                                    AND animal.centre_id = (SELECT centre.centre_id FROM centre WHERE centre.centre_name = 'Australia Zoo')
+                                                                                                    AND animal.spec_name = (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil'))),
+                                                                                            (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil'),
+                                                                                            (SELECT species.spec_name FROM species WHERE species.spec_popular_name = 'Tasmanian Devil'));
+
+Insert into ANIMAL (ANIMAL_ID,ANIMAL_SEX,CENTRE_ID,BREVENT_ID,SPEC_GENUS,SPEC_NAME) values (animal_id_seq.nextval,
+                                                                                            'M',
+                                                                                            (SELECT centre.centre_id FROM centre WHERE centre.centre_name = 'Australia Zoo'),
+                                                                                            (SELECT breeding_event.brevent_id FROM breeding_event WHERE breeding_event.brevent_date = '10-Feb-2021'
+                                                                                                AND breeding_event.father_id = (SELECT animal.animal_id FROM animal WHERE animal.spec_genus = (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil') 
+                                                                                                    AND animal.centre_id = (SELECT centre.centre_id FROM centre WHERE centre.centre_name = 'Werribee Open Range Zoo')
+                                                                                                    AND animal.spec_name = (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil'))),
+                                                                                            (SELECT species.spec_genus FROM species WHERE species.spec_popular_name = 'Tasmanian Devil'),
+                                                                                            (SELECT species.spec_name FROM species WHERE species.spec_popular_name = 'Tasmanian Devil'));
+                                                                                            
+                                                                                            
