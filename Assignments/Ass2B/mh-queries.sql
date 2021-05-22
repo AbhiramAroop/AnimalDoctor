17e07ac16ef69388f8244cb17558637592d077ca
aaro0006 <aaro0006@MVAZ2STUL01049.ad.monash.edu>
2021-05-22 14:03:49 +1000

    Question 2 and 3 of Task1 completed

--- Assignments/Ass2B/mh-queries.sql
+++ Assignments/Ass2B/mh-queries.sql
@@ -24,7 +24,7 @@ SELECT
     mh.endorsement.emp_nbr,
     mh.employee.emp_lname,
     mh.employee.emp_fname,
-    TO_CHAR(TO_DATE(mh.endorsement.end_last_annual_review),'Dy DD Month YYYY')   "REVIEW_DATE"
+    TO_CHAR(TO_DATE(mh.endorsement.end_last_annual_review),'Dy DD Mon YYYY')   "REVIEW_DATE"
 FROM
          mh.endorsement
     INNER JOIN mh.employee
@@ -40,12 +40,54 @@ ORDER BY
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
 
+SELECT
+    mh.charter.charter_nbr,
+    mh.charter.client_nbr,
+    mh.client.client_lname,
+    mh.client.client_fname,
+    mh.charter.charter_special_reqs
+FROM
+         mh.charter
+    INNER JOIN mh.client
+    ON mh.charter.client_nbr = mh.client.client_nbr
+WHERE
+    mh.charter.charter_special_reqs IS NOT null
+ORDER BY
+    mh.charter.charter_nbr;
+
+
+
 
 /*
     Q3
 */
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE your query has a semicolon (;) at the end of this answer
+
+SELECT
+    mh.charter_leg.charter_nbr,
+    concat(mh.client.client_fname, concat(' ', mh.client.client_lname)) fullname,
+    mh.charter.charter_cost_per_hour
+FROM
+         mh.client
+    INNER JOIN mh.charter
+    ON mh.client.client_nbr = mh.charter.client_nbr
+    INNER JOIN mh.charter_leg
+    ON mh.charter.charter_nbr = mh.charter_leg.charter_nbr
+    INNER JOIN mh.location
+    ON mh.charter_leg.location_nbr_destination = mh.location.location_nbr
+WHERE
+    mh.charter_leg.location_nbr_destination = (
+        SELECT
+            mh.location.location_nbr
+        FROM
+            mh.location
+        WHERE
+            mh.location.location_name = 'Mount Doom'
+    ) AND ((mh.charter.charter_cost_per_hour < 1000)
+    OR ( mh.charter.charter_special_reqs IS NULL ))
+ORDER BY
+    fullname DESC;
 
 
 /*
